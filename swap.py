e4c77f2b54864f07b46f25ba54dc362449e95476
Dipendra110 <bhandaridipendra222@gmail.com>
2024-05-26 22:05:33 +0545

    Swap_no.

---
+++ swap.py
@@ -0,0 +1,9 @@
+### Swap Two Numbers
+
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+print(f"Before Swap:\na:{a}\nb:{b}")
+temp=a
+a=b
+b=temp
+print(f"AFter Swap:\na:{a}\nb:{b}")
